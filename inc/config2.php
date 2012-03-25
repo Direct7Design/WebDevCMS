c193251573733079e29c7423ecacb94097d4e0db
Baki Goxhaj <banago@gmail.com>
2012-03-26 00:09:38 +0200

    Last changes

--- inc/config2.php
+++ inc/config2.php
@@ -10,9 +10,8 @@ define('DB', 'webdev');
 $db = mysql_connect( HOST, USER, PASS ) or die('Cannot Connect');
 		mysql_select_db( DB );
 
-// Fillojme sesionin
 session_start();
 
-define('SITENAME' , 'Web Dev'); 
+define('SITENAME' , 'Web Dev CMS'); 
 define('BASEURL', 'http://banago.loc/webdevcms');
 ?>
